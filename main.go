f57a00288a53828d48db13fcd148effddcf840a8
Ezequiel Barrios <ezequiel@barrios.com.ar>
2017-12-25 18:02:34 -0300

    fixed with WaitGroups

--- main.go
+++ main.go
@@ -2,19 +2,36 @@ package main
 
 import(
 	"github.com/lbarrios/logging/logger"
+	"sync"
 )
 
 func main() {
-	log := new(logger.Logger)
+
+	var wg sync.WaitGroup
+	var log logger.Logger
 	log.Init()
 
 	log.Println("Testing 1")
-	go log.Println("Testing 2")
 
-	output(log, 3)
-	go output(log, 4)
+	wg.Add(1)
+	go func(log *logger.Logger, wg *sync.WaitGroup) {
+		defer wg.Done()
+		log.Println("Testing 2")
+	}(&log, &wg)
+
+	output(&log, 3)
+
+	wg.Add(1)
+	go outputRoutine(&log, &wg, 4)
+
+	wg.Wait()
 }
 
 func output(log *logger.Logger, d int){
+	log.Println("Holi", d)
+}
+
+func outputRoutine(log *logger.Logger, wg *sync.WaitGroup, d int){
+	defer wg.Done()
 	log.Println("Holi", d)
 }
